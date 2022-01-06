0ddaea5a74cb2858ac5d99a48b0f0f8e7ad82363
Felix Roos <roos@entrecode.de>
2022-01-06 18:51:45 +0100

    error handling

--- components/common/CodeMirror.tsx
+++ components/common/CodeMirror.tsx
@@ -2,11 +2,15 @@ import React from 'react';
 import { Controlled as CodeMirror2 } from 'react-codemirror2';
 import canUseDOM from '../canUseDOM';
 if (canUseDOM()) {
-  require('codemirror/lib/codemirror.css');
-  require('codemirror/theme/material.css');
-  require('codemirror/theme/neat.css');
-  require('codemirror/mode/xml/xml.js');
-  require('codemirror/mode/javascript/javascript.js');
+  try {
+    require('codemirror/lib/codemirror.css');
+    require('codemirror/theme/material.css');
+    require('codemirror/theme/neat.css');
+    require('codemirror/mode/xml/xml.js');
+    require('codemirror/mode/javascript/javascript.js');
+  } catch (error) {
+    console.log('cannot load codemirror css', error);
+  }
 }
 
 export default function CodeMirror({ value, onChange, options }: any) {
