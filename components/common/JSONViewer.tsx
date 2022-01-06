c95dd84582ce33f48938a2577ba71e2c4c55d8c2
Felix Roos <roos@entrecode.de>
2022-01-06 18:53:33 +0100

    change theme + add flags

--- components/common/JSONViewer.tsx
+++ components/common/JSONViewer.tsx
@@ -3,5 +3,9 @@ import canUseDOM from '../canUseDOM';
 const ReactJson = canUseDOM() ? require('react-json-view').default : null;
 
 export default function JSONViewer(props) {
-  return ReactJson ? <ReactJson {...props} theme="monokai" /> : <>not a browser</>;
+  return ReactJson ? (
+    <ReactJson {...props} theme="tomorrow" displayObjectSize={false} displayDataTypes={false} />
+  ) : (
+    <>not a browser</>
+  );
 }
