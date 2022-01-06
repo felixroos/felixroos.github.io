5f3f8a0ef99aeab5aa7a3543d520b99a4444a163
Felix Roos <roos@entrecode.de>
2022-01-06 21:10:53 +0100

    add img dimensions

--- components/layout/Img.tsx
+++ components/layout/Img.tsx
@@ -1,10 +1,9 @@
 import NextImage from 'next/image';
-import { useState } from 'react';
 
 function Img({ src, alt }: { width: number; height: number; src: string; alt?: string }) {
   let [url, q = ''] = src.split('?');
   url = url.startsWith('.') ? url.slice(1) : url;
-  const [width, height] = q.split('&').map((s) => s.split('=')[1]);
+  const [width, height] = q.split('x');
   // console.log(width, height, url);
   if (!width || !height || url.startsWith('http')) {
     return <img src={url} alt={alt} width={width} height={height} />;
