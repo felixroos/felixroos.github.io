2c010d93b1e946f5d218a988407df27172c90002
Felix Roos <flix91@gmail.com>
2022-07-31 23:47:01 +0200

    fix build?

--- components/rhythmical/features/transpose.ts
+++ components/rhythmical/features/transpose.ts
@@ -1,19 +1,21 @@
 import { curry } from 'ramda';
 import { Note } from '@tonaljs/tonal';
-import { editLeaf, walkRhythmLeafs } from '../tree/RhythmZipper';
+// import { editLeaf, walkRhythmLeafs } from '../tree/RhythmZipper';
 
-
-export const transpose = curry((interval, rhythm) => walkRhythmLeafs(
+/* export const transpose = curry((interval, rhythm) => walkRhythmLeafs(
   editLeaf(leaf => Note.midi(leaf) ? Note.transpose(leaf, interval) : leaf),
   rhythm
-))
+)) */
 
-export default transpose;
+// export default transpose;
 
-export const transposeEvent = curry((interval, { value, transpose, ...event }) => ({
-  ...event,
-  value: Note.midi(value) ? Note.transpose(value, interval) : value,
-}));
+export const transposeEvent = curry(
+  (interval, { value, transpose, ...event }) => ({
+    ...event,
+    value: Note.midi(value) ? Note.transpose(value, interval) : value,
+  })
+);
 
 export const transposeEvents = curry((interval, events) =>
-  events.map(transposeEvent(interval)))
+  events.map(transposeEvent(interval))
+);
--- components/rhythmical/runtime.ts
+++ components/rhythmical/runtime.ts
@@ -1,25 +1,25 @@
 import { rhythmEvents } from './tree/hierarchy';
 import { flatEvents } from './tree/walker';
-import { renderEvents } from './tree/RhythmZipper';
+// import { renderEvents } from './tree/RhythmZipper';
 import { renderRhythm } from './rhythmical';
 import tetris from './tunes/tetris';
 
 function runtime(fn) {
-  const t0 = performance.now()
+  const t0 = performance.now();
   const result = fn();
-  const t1 = performance.now()
+  const t1 = performance.now();
   console.log('result', result);
   return t1 - t0;
 }
 
 function test() {
-  const d3Time = runtime(() => rhythmEvents(tetris))
+  const d3Time = runtime(() => rhythmEvents(tetris));
   console.log('d3Time', d3Time);
-  const superTime = runtime(() => flatEvents(tetris))
+  const superTime = runtime(() => flatEvents(tetris));
   console.log('superTime', superTime);
-  const newTime = runtime(() => renderEvents(tetris))
-  console.log('newTime', newTime);
-  const oldTime = runtime(() => renderRhythm(tetris))
+  // const newTime = runtime(() => renderEvents(tetris))
+  // console.log('newTime', newTime);
+  const oldTime = runtime(() => renderRhythm(tetris));
   console.log('oldTime', oldTime);
 }
 
