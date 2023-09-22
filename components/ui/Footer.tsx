8c6052b5bca9c47688fbc59f17acfd3c071725b3
Felix Roos <flix91@gmail.com>
2023-09-22 17:37:59 +0200

    add mastodon + github links

--- components/ui/Footer.tsx
+++ components/ui/Footer.tsx
@@ -1,10 +1,18 @@
 const Footer = (props) => (
   <div className="py-4 font-serif bg-slate-900 dark:bg-slate-800 text-white flex justify-center items-center">
     <p className="leading-none">
+      ❤️ {new Date().getFullYear()}{' '}
       <a className="underline" href="https://github.com/felixroos">
         Felix Roos
       </a>{' '}
-      {new Date().getFullYear()}
+      |{' '}
+      <a rel="me" href="https://post.lurk.org/@froos">
+        mastodon
+      </a>{' '}
+      |{' '}
+      <a rel="me" href="https://github.com/felixroos/">
+        github
+      </a>
     </p>
   </div>
 );
